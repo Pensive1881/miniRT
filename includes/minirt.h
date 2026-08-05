8bb1d9b575c659ba38bb87f8bd53c4cbf8455811
Pensive <lumberjackparade@hotmail.com>
2026-08-05 14:45:15 +0200

    defined the core data structures: vec3, color, ambient, camera, light, sphere, scene in minirt.h

--- includes/minirt.h
+++ includes/minirt.h
@@ -4,6 +4,43 @@
 # include <math.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdio.h>
+# include <string.h>
 # include "mlx.h"
+
+typedef struct s_vec3
+{
+
+}   t_vec3;
+
+typedef struct s_color
+{
+
+}   t_color;
+
+typedef struct s_ambient
+{
+
+}   t_ambient;
+
+typedef struct s_camera
+{
+
+}   t_camera;
+
+typedef struct s_light
+{
+
+}   t_light;
+
+typedef stfuct s_sphere
+{
+
+}   t_sphere;
+
+typedef struct s_scene
+{
+
+}   t_scene;
 
 #endif
