bba24ea79bc900e73b961b175e0fddf0f867c943
Pensive <lumberjackparade@hotmail.com>
2026-08-05 15:10:02 +0200

    created scene initializer, number parser and vector parser in main.c

--- main.c
+++ main.c
@@ -1,1 +1,49 @@
 #include "minirt.h"
+
+// scene initializer: resets the scene to zero
+static void init_scene(t_scene *scene)
+{
+    memset(scene, 0, sizeof(*scene));
+}
+
+// number parser
+static int  parse_double(const char *str, double *out)
+{
+    char    *end;
+
+    *out = strtod(str, &end);
+
+    return (end != str && *end== '\0');
+}
+
+// vector parser: camera positions/directions and sphere centers
+static int  parse_vec3(const char *str, t_vec *out)
+{
+    char    *token;
+    char    tmp[128];
+    int     i;
+
+    if (strlen(str) >= sizeof(tmp))
+        return (0);
+    strcpy(tmp, str);
+
+    token = strtok(tmp, ",");
+    if (!token)
+        return (0);
+    out->x = atof(token);
+
+    i = 1;
+    while (i <=2)
+    {
+        token = strtok(NULL, ",");
+        if (!token)
+            return (0);
+        if (i == 1)
+            out->y = atog(token);
+        else
+            out->z = atof(token);
+        i++:
+    }
+
+    return (1);
+}
