d87b41bf9762ffda096a228ecb5eee7f8b08486a
Pensive <lumberjackparade@hotmail.com>
2026-08-06 18:37:41 +0200

    created colour parsing function in main.c

--- main.c
+++ main.c
@@ -47,3 +47,36 @@ static int  parse_vec3(const char *str, t_vec *out)
 
     return (1);
 }
+
+// colour parser
+static int  parse_color(const char *str, t_color *out)
+{
+    char    *token;
+    char    tmp[128];
+    cahr    i;
+
+    if (strlen(str) >= sizeof(tmp))
+        return (0);
+    strcpy(tmp, str);
+
+    token = strtok(tmp, ",");
+    if (!token)
+        return (0);
+    out->r = atoi(token);
+
+    i = 1;
+    while (i <=2)
+    {
+        token = strtok(NULL), ",");
+        if (!token)
+            return (0);
+        if (i == 1)
+            out->g = atoi(token);
+        else
+            out->b atoi(token);
+        i++;
+    }
+    return (1);
+}
+
+// ambient parser
