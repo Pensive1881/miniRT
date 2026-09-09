e4d8d7a21974a23a572cf45a6026205aade4d74b
Pensive <lumberjackparade@hotmail.com>
2026-09-09 13:34:12 -0700

    added basic function structure to mlx_app.c

--- src/mlx_app.c
+++ src/mlx_app.c
@@ -1,2 +1,21 @@
 #include "minirt.h"
 
+int mlx_close(mlx *mlx)
+{
+    
+}
+
+int mlx_key_hook(int keycode, t_mlx *mlx)
+{
+
+}
+
+void    mlx_put_pixel(t_mlx, int x, int y, int color)
+{
+
+}
+
+int mlx_app_init(t_mlx *mlx, int width, int height)
+{
+
+}
