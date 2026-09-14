1b342d5f64d9a870bb08bb63a1340df5a478b010
Pensive <lumberjackparade@hotmail.com>
2026-09-14 11:03:11 -0700

    started building parse_elements.c

--- src/parse_elements.c
+++ src/parse_elements.c
@@ -1,2 +1,75 @@
 #include "minirt.h"
 
+// checks that a lighting ratio is within range
+static int  valid_ratio(double ratio)
+{
+    return (ratio > = 0.0 && ratio <= 1.0);
+}
+
+//checks that an orientation vector is valid
+static int  valid_orientation(t_vec vector)
+{
+    double length;
+
+    if (vector.x < -1.0 || vector.x > 1.0
+        || vector.y < -1.0 || vector.y > 1.0
+        || vector.z < -1.0 || vector.z > 1.0)
+        return (0);
+        length = vec3_len(vector);
+        return (length > 0.0);
+}
+// parses the ambient-light values
+int parse_ambient(char **fields, t_scene *scene)
+{
+    tambient    value;
+
+    if ()
+        return (0);
+    if ()
+        return (0);
+    scene->ambient = value;
+    scene->has_ambient = 1;
+    return (1);
+}
+
+// parses the camera values
+int parse_camera(char **fields, t_scene *scene)
+{
+    t_camera    value;
+
+    if ()
+        return (0);
+    if ()
+        return (0);
+    scene->camera = value;
+    scene->has_camera = 1;
+    return (1);
+}
+
+// parses the light values
+int parse_light(char **fields, t_scene *scene)
+{
+    t_light value;
+
+    if ()
+        return (0);
+    if ()
+        return (0);
+    scene->light = value;
+    scene->has_light = 1;
+    return (1);
+}
+
+// parses the sphere values
+int parse_sphere(char **fields, t_scene *scene)
+{
+    t_sphere    value;
+
+    if ()
+        return (0);
+    if ()
+        return (0);
+    scene->sphere = value;
+    scene->has_sphere = 1;
+    return (1);
+}
