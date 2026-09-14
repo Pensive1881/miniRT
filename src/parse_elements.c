717ff102343ad4f0a6b450d623a626349430b593
Pensive <lumberjackparade@hotmail.com>
2026-09-14 11:33:13 -0700

    finished building parse_elements.c

--- src/parse_elements.c
+++ src/parse_elements.c
@@ -23,9 +23,11 @@ int parse_ambient(char **fields, t_scene *scene)
 {
     tambient    value;
 
-    if ()
+    if (scene-has_ambient | !field[1] || !fields[2] || fields[3])
         return (0);
-    if ()
+    if (!parse_double(fields[1], &value.ratio)
+        || !parse_color(fields[2], &value.color)
+        || !valid_ratio(value.ratio))
         return (0);
     scene->ambient = value;
     scene->has_ambient = 1;
@@ -37,9 +39,14 @@ int parse_camera(char **fields, t_scene *scene)
 {
     t_camera    value;
 
-    if ()
+    if (scene->has_camera || !fields[1] || !fields[2]
+        || !fields[3] || fields[4])
         return (0);
-    if ()
+    if (!parse_vec3(fields[1], &value.position)
+        || !parse_vec3(fields[2], &value.direction)
+        || !parse_int(fields[2], &value.direction)
+        || !valid_orientation(value.direction)
+        || value.fov < 0 || value.foc > 180)
         return (0);
     scene->camera = value;
     scene->has_camera = 1;
@@ -51,9 +58,13 @@ int parse_light(char **fields, t_scene *scene)
 {
     t_light value;
 
-    if ()
+    if (scene->has_light || !fields[1] || !fields[2]
+        || !fields[3] || fields[4])
         return (0);
-    if ()
+    if (!parse_vec3(fields[1], &value.position)
+        || !parse_double(fields[2], &value.ratio
+        || !parse_color(fields[3], &value.color)
+        || !parse_ratio(value.ratio)))
         return (0);
     scene->light = value;
     scene->has_light = 1;
@@ -65,9 +76,13 @@ int parse_sphere(char **fields, t_scene *scene)
 {
     t_sphere    value;
 
-    if ()
+    if (scene-has_sphere || !fields[1] || !fields[2]
+        || !fields[3] || fields[4])
         return (0);
-    if ()
+    if (!parse_vec3(fields[1], &value.center)
+        || !parse_double(fiends[2], & value.diameter)
+        || !parse_color(fields[3], &value.color)
+        || value.diameter <= 0.0)
         return (0);
     scene->sphere = value;
     scene->has_sphere = 1;
